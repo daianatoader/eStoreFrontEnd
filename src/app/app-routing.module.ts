ee17de1cfd414a439478b41df3980573b763cb3a
Ionut Stetco <ionut.stetco.bp@nttdata.ro>
2017-08-11 13:47:10 +0300

    fixed brand routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {BrandsComponent} from './brand/brands.component';
 import {BrandDetailComponent} from './brand/brand-detail.component';
 import {AdminsComponent} from './admin/admin.component';
 import {AdminDetailComponent} from './admin/admin-detail.component';
@@ -22,6 +23,7 @@ const routes: Routes = [
   {path: 'detail/:id', component: HeroDetailComponent},
   {path: 'brandDetail/:id', component: BrandDetailComponent},
   {path: 'heroes', component: HeroesComponent},
+  {path: 'brands', component: BrandsComponent},
   {path: 'admins', component: AdminsComponent},
   {path: 'sections', component: SectionsComponent},
   {path: 'sectionDetail/:id', component: SectionDetailComponent},
