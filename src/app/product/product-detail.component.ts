f4e1271e8e0eb435a1298456d5287e7a15755f94
Daiana Toader <toaderdaiana95@yahoo.com>
2017-08-17 11:03:43 +0300

    photo upload for product

--- src/app/product/product-detail.component.ts
+++ src/app/product/product-detail.component.ts
@@ -1,7 +1,8 @@
 import 'rxjs/add/operator/switchMap';
-import { Component, OnInit }        from '@angular/core';
+import { Component, OnInit,ElementRef }        from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
+import { Response, Http } from '@angular/http';
 
 import { Product }        from './product';
 import { ProductService } from './product.service';
@@ -13,11 +14,14 @@ import { ProductService } from './product.service';
 })
 export class ProductDetailComponent implements OnInit {
     product: Product;
+    private uploadUrl = 'http://localhost:8080/update';
 
     constructor(
         private productService: ProductService,
         private route: ActivatedRoute,
-        private location: Location
+        private location: Location,
+        private el: ElementRef,
+        private http: Http
     ) {}
 
     ngOnInit(): void {
@@ -25,6 +29,25 @@ export class ProductDetailComponent implements OnInit {
             .switchMap((params: ParamMap) => this.productService.getProduct(+params.get('id')))
             .subscribe(product => this.product = product);
     }
+
+      upload() {
+        let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
+        let fileCount: number = inputEl.files.length;
+        let formData = new FormData();
+        if (fileCount > 0) { // a file was selected
+            formData.append('photo', inputEl.files.item(0));
+            this.http.post(this.uploadUrl, formData)
+            .toPromise().then(this.informUser).catch(this.handleError);
+          }
+       }
+
+  informUser(res: Response): void {
+    alert('image ' + res.text() + ' successfully uploaded');
+  }
+
+  handleError(): void {
+    alert('eroare');
+  }
 
     save(): void {
         this.productService.update(this.product)
--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -3,4 +3,5 @@ export class Product {
   name: string;
   details: string;
   price: number;
+  photoPath: string;
 }
